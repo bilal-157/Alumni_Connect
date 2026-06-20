b83a01bebc6911512387eb21f4cd8bc3671b3caa
Bilal Rafique <rafiqueb087@gmail.com>
2026-06-20 05:07:44 +0500

    Alumni System Is Created !

--- config/mail.php
+++ config/mail.php
@@ -2,11 +2,11 @@
 // config/mail.php
 return [
     // For Gmail (recommended for testing)
-    'host' => 'smtp.gmail.com',
-    'port' => 587,
-    'username' => 'rafiqueb087@gmail.com',  // CHANGE THIS
-    'password' => 'szgyewjlkimrahoo',      // CHANGE THIS (NOT your regular password)
-    'encryption' => 'tls',
-    'from_email' => 'noreply@yourdomain.com',
-    'from_name' => 'Alumni Connect System'
+    'host' => '',
+    'port' => '',
+    'username' => '',  // CHANGE THIS
+    'password' => '',      // CHANGE THIS (NOT your regular password)
+    'encryption' => '',
+    'from_email' => '@yourdomain.com',
+    'from_name' => ''
 ];
